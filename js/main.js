8e4c56c0cfd9e1d693cb9126ef99a80cb9044ff6
Nick-Hericks <nickhericks@gmail.com>
2019-02-13 17:09:38 +0200

    Add event listeners

--- js/main.js
+++ js/main.js
@@ -1,2 +1,25 @@
 // START EDITING YOUR JAVASCRIPT HERE
 // ===============
+
+const openButton = document.querySelector('.jsModalToggle');
+const closeButton = document.querySelector('.jsModalClose')
+const body = document.querySelector('body');
+
+
+// Having an event listener on teh entire page allows us to
+// close the modal by clicking anywhere on the body after it is open.
+body.addEventListener('click', function(event) {
+  console.log(event.target);
+  if (event.target === openButton) {
+    console.log(event.target);
+    body.classList.add('modal-is-open');
+  }
+  else if (body.classList.contains('modal-is-open')){
+    body.classList.remove('modal-is-open');
+  }
+});
+
+
+closeButton.addEventListener('click', _ => {
+  document.body.classList.remove('modal-is-open')
+})
