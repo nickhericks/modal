595b46e2411d03e936a03878ce6e2e13d20a0338
Nick-Hericks <nickhericks@gmail.com>
2019-02-28 17:45:10 +0200

    Fix js file tabs

--- js/main.js
+++ js/main.js
@@ -2,24 +2,24 @@
 // ===============
 
 const openButton = document.querySelector('.jsModalToggle');
-const closeButton = document.querySelector('.jsModalClose')
+const closeButton = document.querySelector('.jsModalClose');
 const body = document.querySelector('body');
 
 
 // Having an event listener on the entire page allows us to
 // close the modal by clicking anywhere on the body after it is open.
 body.addEventListener('click', function(event) {
-  console.log(event.target);
-  if (event.target === openButton) {
-    console.log(event.target);
-    body.classList.add('modal-is-open');
-  }
-  else if (body.classList.contains('modal-is-open')){
-    body.classList.remove('modal-is-open');
-  }
+	console.log(event.target);
+	if (event.target === openButton) {
+		console.log(event.target);
+		body.classList.add('modal-is-open');
+	}
+	else if (event.target === openButton){
+		body.classList.remove('modal-is-open');
+	}
 });
 
 
 closeButton.addEventListener('click', _ => {
-  document.body.classList.remove('modal-is-open')
-})
+	document.body.classList.remove('modal-is-open');
+});
