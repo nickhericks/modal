9193c865e2ca7fc87048e09da2c40608072f7f20
Nick-Hericks <nickhericks@gmail.com>
2019-02-28 18:28:29 +0200

    add event listener to modalContainer

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@
 const openButton = document.querySelector('.jsModalToggle');
 const closeButton = document.querySelector('.jsModalClose');
 const body = document.querySelector('body');
+const modalContainer = document.querySelector('.modal-container');
 
 
 // Having an event listener on the entire page allows us to
@@ -14,7 +15,7 @@ body.addEventListener('click', function(event) {
 		console.log(event.target);
 		body.classList.add('modal-is-open');
 	}
-	else if (event.target === openButton){
+	else if (event.target === modalContainer){
 		body.classList.remove('modal-is-open');
 	}
 });
