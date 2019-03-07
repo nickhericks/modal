fd3b6411e8b24cd9c4827d9767719f9fd45a576f
Nick-Hericks <nickhericks@gmail.com>
2019-03-07 18:50:34 +0200

    Final wave animation

--- js/main.js
+++ js/main.js
@@ -1,34 +1,36 @@
-// START EDITING YOUR JAVASCRIPT HERE
+/* globals TimelineMax Back */
+// # START EDITING YOUR JAVASCRIPT HERE
 // ===============
 
-const openButton = document.querySelector('.jsModalToggle');
-const closeButton = document.querySelector('.jsModalClose');
-const body = document.querySelector('body');
-const modal = document.querySelector('.modal')
+const toggleButton = document.querySelector('.jsModalToggle')
+const closeButton = document.querySelector('.jsModalClose')
 const container = document.querySelector('.modal-container')
+const hand = document.querySelector('.wave-hand')
 
+const wave = _ => {
+	const tl = new TimelineMax({})
+	tl.set(hand, { transformOrigin: 'bottom center' })
+	tl.from(hand, 0.5, {
+		scale: 0.5,
+		opacity: 0,
+		ease: Back.easeOut.config(1.5)
+	})
+	tl.to(hand, 0.2, { rotation: 15 })
+	tl.to(hand, 0.2, { rotation: -15 })
+	tl.to(hand, 0.2, { rotation: 15 })
+	tl.to(hand, 0.2, { rotation: -15 })
+	tl.to(hand, 0.2, { rotation: 0 })
+}
 
-// Having an event listener on the entire page allows us to
-// close the modal by clicking anywhere on the body after it is open.
-body.addEventListener('click', function(event) {
-	console.log(event.target);
-	if (event.target === openButton) {
-		console.log(event.target);
-		body.classList.add('modal-is-open');
-	}
-});
-
-// Close modal when close button is clicked
-closeButton.addEventListener('click', _ => {
-	document.body.classList.remove('modal-is-open');
-});
-
-// Don't allow clicks on modal
-modal.addEventListener('click', e => {
-	e.stopPropagation()
+toggleButton.addEventListener('click', _ => {
+	document.body.classList.add('modal-is-open')
+	wave()
 })
 
-// Close modal if modal container is clicked
+closeButton.addEventListener('click', _ => {
+	document.body.classList.remove('modal-is-open')
+})
+
 container.addEventListener('click', e => {
 	if (!e.target.closest('.modal')) {
 		document.body.classList.remove('modal-is-open')
