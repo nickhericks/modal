2484f36293fb4adb9d4327e7e3df5a67c826dc20
Nick-Hericks <nickhericks@gmail.com>
2019-03-12 12:40:29 +0200

    Reorder functions

--- js/main.js
+++ js/main.js
@@ -1,34 +1,35 @@
-/* globals TimelineMax Back */
-// # START EDITING YOUR JAVASCRIPT HERE
-// ===============
+// Open modal
+const openModal = () => {
+  document.body.classList.add("modal-is-open");
+};
+
+// Close modal
+const closeModal = () => {
+  document.body.classList.remove("modal-is-open");
+};
+
+// Wave hand
+const wave = () => {
+  const hand = document.querySelector(".wave-hand");
+  const tl = new TimelineMax({});
+  tl.set(hand, { transformOrigin: "bottom center" });
+  tl.from(hand, 0.5, {
+    scale: 0.5,
+    opacity: 0,
+    ease: Back.easeOut.config(1.5)
+  });
+  tl.to(hand, 0.2, { rotation: 15 });
+  tl.to(hand, 0.2, { rotation: -15 });
+  tl.to(hand, 0.2, { rotation: 15 });
+  tl.to(hand, 0.2, { rotation: -15 });
+  tl.to(hand, 0.2, { rotation: 0 });
+};
+
 
 const toggleButton = document.querySelector('.jsModalToggle');
 const closeButton = document.querySelector('.jsModalClose');
 const container = document.querySelector('.modal-container');
-const hand = document.querySelector('.wave-hand');
 
-const openModal = () => {
-	document.body.classList.add('modal-is-open');
-};
-
-const closeModal = () => {
-	document.body.classList.remove('modal-is-open');
-};
-
-const wave = () => {
-	const tl = new TimelineMax({});
-	tl.set(hand, { transformOrigin: 'bottom center' });
-	tl.from(hand, 0.5, {
-		scale: 0.5,
-		opacity: 0,
-		ease: Back.easeOut.config(1.5)
-	});
-	tl.to(hand, 0.2, { rotation: 15 });
-	tl.to(hand, 0.2, { rotation: -15 });
-	tl.to(hand, 0.2, { rotation: 15 });
-	tl.to(hand, 0.2, { rotation: -15 });
-	tl.to(hand, 0.2, { rotation: 0 });
-};
 
 toggleButton.addEventListener('click', () => {
 	openModal();
