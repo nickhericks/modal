53efdc5b4271dcf1d7c8ade27eda23ab047717ec
Nick-Hericks <nickhericks@gmail.com>
2019-07-31 13:49:34 +0200

    Fix spacing issues

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
 // Open modal
 const openModal = () => {
-  document.body.classList.add("modal-is-open");
+	document.body.classList.add('modal-is-open');
 };
 
 // Close modal
 const closeModal = () => {
-  document.body.classList.remove("modal-is-open");
+	document.body.classList.remove('modal-is-open');
 };
 
 // Wave hand
 const wave = () => {
-  const hand = document.querySelector(".wave-hand");
-  const tl = new TimelineMax({});
-  tl.set(hand, { transformOrigin: "bottom center" });
-  tl.from(hand, 0.5, {
-    scale: 0.5,
-    opacity: 0,
-    ease: Back.easeOut.config(1.5)
-  });
-  tl.to(hand, 0.2, { rotation: 15 });
-  tl.to(hand, 0.2, { rotation: -15 });
-  tl.to(hand, 0.2, { rotation: 15 });
-  tl.to(hand, 0.2, { rotation: -15 });
-  tl.to(hand, 0.2, { rotation: 0 });
+	const hand = document.querySelector('.wave-hand');
+	const tl = new TimelineMax({});
+	tl.set(hand, { transformOrigin: 'bottom center' });
+	tl.from(hand, 0.5, {
+		scale: 0.5,
+		opacity: 0,
+		ease: Back.easeOut.config(1.5)
+	});
+	tl.to(hand, 0.2, { rotation: 15 });
+	tl.to(hand, 0.2, { rotation: -15 });
+	tl.to(hand, 0.2, { rotation: 15 });
+	tl.to(hand, 0.2, { rotation: -15 });
+	tl.to(hand, 0.2, { rotation: 0 });
 };
 
 
